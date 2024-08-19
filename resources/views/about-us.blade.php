db46932fa9d18706cea9e6412b35ccaabbc95674
Andrea Sdringola <andrea@ufficio>
2024-08-19 21:31:41 +0200

    Selfwork Rotte

---
+++ resources/views/about-us.blade.php
@@ -0,0 +1,64 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Chi siamo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="/style.css">
+  </head>
+
+  <body>
+  <nav class="navbar navbar-expand-lg bg-dark border -botton" data-bs-theme="dark">
+  <div class="container-fluid">
+  <a class="navbar-brand" href="/"><i class="bi bi-camera-reels-fill"></i></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="//chisiamo">Chi siamo</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="/contatti">Contatti</a>
+        </li>
+
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        
+      </ul>
+      
+    </div>
+  </div>
+</nav>
+<header>
+    <div class="container-fluid header">
+        <div class="row h-100 justify-content-around align-items-center">
+            <div class="col-6">
+                <h2 class="text-white text-color text-center">Chi Siamo</h2>
+                <p class="text-white text-color">Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit error neque, maiores pariatur quaerat quidem dolore quam, odio, autem ipsum nihil. Esse cupiditate nulla vero sunt beatae maiores ducimus consequatur laborum saepe dignissimos, tenetur, nesciunt voluptatibus impedit assumenda quidem dolorum obcaecati asperiores ab necessitatibus voluptatem accusamus deserunt! Omnis, molestiae inventore!</p>
+            </div>
+            <div class="col-6">
+                <img src="/media/team.jpg" alt="foto del team" class="shadow rounded">
+            </div>
+        </div>
+    </div>
+</header>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
